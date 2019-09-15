1db916d206b9f560830efa0d672bda88646fd7f3
Adolf <lopo@lopo-pc>
2019-09-14 23:16:43 -0500

    environment variables

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7q^7ad!_&t=znwo#l7&&=1du4_jjy*g*g@l_r$k(p8wk33^7*i'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
